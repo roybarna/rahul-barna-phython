ae2fe989922df4c76887c78757ada5886ff4d287
roybarna <117443960+roybarna@users.noreply.github.com>
2023-03-24 08:21:47 +0530

    Add files via upload

---
+++ membarship opreters.py
@@ -0,0 +1,11 @@
+x = ["apple", "banana"]
+
+print("banana" in x)
+
+# returns True because a sequence with the value "banana" is in the list
+x = ["apple", "banana"]
+
+print("banana" in x)
+
+# returns True because a sequence with the value "banana" is in the list
+
